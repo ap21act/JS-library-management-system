b52a28ae138ca1ee57edf4d1838f4894cfdbc4df
Adarsha <paudeladarsha111@gmail.com>
2025-01-14 08:31:56 +0000

    Theming and Home Layout Design

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+interface Book {
+    id: string;
+    title: string;
+    author: string;
+    genre: string;
+    rating: number;
+    coverUrl: string;
+    coverColor: string;
+    description: string;
+    totalCopies: number;
+    availableCopies: number;
+    videoUrl: string;
+    summary: string;
+    isLoanedBook: boolean;
+}
